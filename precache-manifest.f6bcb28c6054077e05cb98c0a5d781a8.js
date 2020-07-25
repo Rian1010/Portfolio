6a3369a9626794bd459409ba826a3d36b468b23e
Rian1010 <rtegally1098@gmail.com>
2020-07-25 16:20:35 +0200

    Updates

---
+++ precache-manifest.f6bcb28c6054077e05cb98c0a5d781a8.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a195864e75bb84c7c9f215a2fa4d297",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "7b2a5845b46c4fbebd1b",
+    "url": "/Portfolio/static/css/main.faf28d7f.chunk.css"
+  },
+  {
+    "revision": "55449f06a4981f08d4e7",
+    "url": "/Portfolio/static/js/2.034b4701.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/Portfolio/static/js/2.034b4701.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7b2a5845b46c4fbebd1b",
+    "url": "/Portfolio/static/js/main.c257e62f.chunk.js"
+  },
+  {
+    "revision": "b83a37198ba343383876",
+    "url": "/Portfolio/static/js/runtime-main.e440ee5a.js"
+  },
+  {
+    "revision": "4217c3481c53ba6309fb89567d5743a9",
+    "url": "/Portfolio/static/media/charity-site.4217c348.png"
+  },
+  {
+    "revision": "289fba2d8728a77b6ce6e43de220a8fa",
+    "url": "/Portfolio/static/media/devices.289fba2d.png"
+  },
+  {
+    "revision": "e1e56d019f8fdeb08fe40988759340f1",
+    "url": "/Portfolio/static/media/germany-site.e1e56d01.png"
+  },
+  {
+    "revision": "ea45f0014964faa0e8b2a6beab40c30f",
+    "url": "/Portfolio/static/media/js-site.ea45f001.png"
+  },
+  {
+    "revision": "8ec2085e6a2d0f2194c12d640b67a4b7",
+    "url": "/Portfolio/static/media/phoneSite.8ec2085e.png"
+  },
+  {
+    "revision": "773b92e567ef7cba20d75d0418781b49",
+    "url": "/Portfolio/static/media/profile-pic.773b92e5.jpeg"
+  },
+  {
+    "revision": "0fbd4722729889f917f064ca2f73f81d",
+    "url": "/Portfolio/static/media/shopping-list.0fbd4722.png"
+  },
+  {
+    "revision": "35ffa97a2d677f7b590bc9fe93eee7ee",
+    "url": "/Portfolio/static/media/skywheelers-site.35ffa97a.png"
+  }
+]);
