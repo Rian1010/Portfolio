5eb65eb1417ff092949bef56cac0e9d5579e9a0e
Rian1010 <rtegally1098@gmail.com>
2021-02-13 19:09:25 +0100

    Added another website to the portfolio

--- portfolio/src/full-portfolio.js
+++ portfolio/src/full-portfolio.js
@@ -1,84 +1,92 @@
-const fullPortfolio = [
-{
-    img: "site-1",
-    title: "Amazon Clone",
-    titel: "Amazon Klon",
-    technologies: "ReactJS, stripe API, CSS, HTML, Redux, Express, Firebase, Github",
-    sourceCode: "https://github.com/Rian1010/Amazon-Clone",
-    websiteURL: "https://clone-568b3.web.app/"
-},
-{
-    img: "site-2",
-    title: "E-Commerce Website - Phone Shop (Fourth Milestone Project)",
-    titel: "E-Commerce-Website - Phone Shop (Viertes Meilenstein Projekt)",
-    technologies: "Django, Python, PostgresSQL, JavaScript, jQuery, stripe API, Bootstrap, CSS, HTML, Github, Heroku",
-    sourceCode: "https://github.com/Rian1010/Milestone4",
-    websiteURL: "https://milestone4-django-phone-shop.herokuapp.com/"
-},
-{
-    img: "site-3",
-    title: "Mainhatten Skywheelers (Still in Progress)",
-    titel: "Mainhatten Skywheelers (Noch in Bearbeitung)",
-    technologies: "Django, Python, PostgresSQL, JavaScript, jQuery, Bootstrap, CSS, HTML, Email.js, YouTube Live Stream API, Github, Heroku",
-    sourceCode: "https://github.com/Rian1010/Mainhatten-Skywheelers",
-    websiteURL: ""
-},
-{
-    img: "site-4",
-    title: "Boutique Ado",
-    titel: "Boutique Ado",
-    technologies: "Python, Django, PostgresSQL, stripe API, JavaScript, jQuery, CSS, Bootstrap, HTML, Github, Heroku",
-    sourceCode: "https://github.com/Rian1010/boutique_ado",
-    websiteURL: "https://rian-boutique-ado.herokuapp.com/"
-},
-{
-    img: "site-5",
-    title: "Language Learner's App / JavaScript Learning App (Third Milestone Project)",
-    titel: "Sprachlerner App / JavaScript-Lern-App (Drittes Meilenstein Projekt)",
-    technologies: "Flask, Python, MongoDB, JavaScript, jQuery, Email.js API, Bootstrap, CSS, HTML, Github, Heroku",
-    sourceCode: "https://github.com/Rian1010/Language-Learners-App",
-    websiteURL: "https://language-learners-app.herokuapp.com/"
-},
-{
-    img: "site-6",
-    title: "Shopping-List",
-    titel: "Einkaufsliste",
-    technologies: "Flask, Python, MongoDB, JavaScript, jQuery, Bootstrap, CSS, HTML, Github, Heroku",
-    sourceCode: "https://github.com/Rian1010/Shopping-List",
-    websiteURL: "https://shopping-list-google.herokuapp.com/"
-},
-{
-    img: "site-7",
-    title: "Charity Base (Second Milestone Project)",
-    titel: "Wohltätigkeiten Website",
-    technologies: "HTML, CSS, JavaScript, DC.js API, D3.js API, Email.js API, Google Maps API, Github",
-    sourceCode: "https://github.com/Rian1010/Charity-Website",
-    websiteURL: "https://rian1010.github.io/Charity-Website/"
-}, 
-{
-    img: "site-8",
-    title: "Portfolio",
-    titel: "Portfolio",
-    technologies: "ReactJS, JavaScript, Bootstrap, Responsive Web Design, HTML, CSS, Github",
-    sourceCode: "https://github.com/Rian1010/Portfolio",
-    websiteURL: "https://rian1010.github.io/Portfolio/"
-},
-{
-    img: "site-9",
-    title: "Thorin & Company",
-    titel: "Thorin & Die Truppe",
-    technologies: "Flask, Python, AJAX, jQuery, JavaScript, Bootstrap, HTML, CSS, SASS, Github, Heroku",
-    sourceCode: "https://github.com/Rian1010/Flask",
-    websiteURL: "https://thorin-and-company-4.herokuapp.com/"
-},
-{
-    img: "site-10",
-    title: "Germany Online Guide (First Milestone Project)",
-    titel: "Deutschland Online-Guide (Erstes Meilenstein Projekt)",
-    technologies: "HTML, CSS, Bootstrap, JavaScripts, Github",
-    sourceCode: "https://github.com/Rian1010/Germany-Online-Guide",
-    websiteURL: "https://rian1010.github.io/Germany-Online-Guide/index.html"
-}
+const fullPortfolio = [{
+        img: "site-1",
+        title: "TravelGoal",
+        titel: "TravelGoal",
+        technologies: "Vanilla JavaScript, Google Maps API, HTML, CSS, Git, Github",
+        sourceCode: "https://github.com/Rian1010/Travel-Website",
+        websiteURL: "https://rian1010.github.io/Travel-Website/"
+    },
+    {
+        img: "site-2",
+        title: "E-Commerce Website - Phone Shop (Fourth Milestone Project)",
+        titel: "E-Commerce-Website - Phone Shop (Viertes Meilenstein Projekt)",
+        technologies: "Django, Python, PostgresSQL, JavaScript, jQuery, stripe API, Bootstrap, CSS, HTML, Github, Heroku",
+        sourceCode: "https://github.com/Rian1010/Milestone4",
+        websiteURL: "https://milestone4-django-phone-shop.herokuapp.com/"
+    },
+    {
+        img: "site-3",
+        title: "Mainhatten Skywheelers (Still in Progress)",
+        titel: "Mainhatten Skywheelers (Noch in Bearbeitung)",
+        technologies: "Django, Python, PostgresSQL, JavaScript, jQuery, Bootstrap, CSS, HTML, Email.js, YouTube Live Stream API, Github, Heroku",
+        sourceCode: "https://github.com/Rian1010/Mainhatten-Skywheelers",
+        websiteURL: ""
+    },
+    {
+        img: "site-4",
+        title: "Amazon Clone",
+        titel: "Amazon Klon",
+        technologies: "ReactJS, stripe API, CSS, HTML, Redux, Express, Firebase, Github",
+        sourceCode: "https://github.com/Rian1010/Amazon-Clone",
+        websiteURL: "https://clone-568b3.web.app/"
+    },
+    {
+        img: "site-5",
+        title: "Boutique Ado",
+        titel: "Boutique Ado",
+        technologies: "Python, Django, PostgresSQL, stripe API, JavaScript, jQuery, CSS, Bootstrap, HTML, Github, Heroku",
+        sourceCode: "https://github.com/Rian1010/boutique_ado",
+        websiteURL: "https://rian-boutique-ado.herokuapp.com/"
+    },
+    {
+        img: "site-6",
+        title: "Language Learner's App / JavaScript Learning App (Third Milestone Project)",
+        titel: "Sprachlerner App / JavaScript-Lern-App (Drittes Meilenstein Projekt)",
+        technologies: "Flask, Python, MongoDB, JavaScript, jQuery, Email.js API, Bootstrap, CSS, HTML, Github, Heroku",
+        sourceCode: "https://github.com/Rian1010/Language-Learners-App",
+        websiteURL: "https://language-learners-app.herokuapp.com/"
+    },
+    {
+        img: "site-7",
+        title: "Shopping-List",
+        titel: "Einkaufsliste",
+        technologies: "Flask, Python, MongoDB, JavaScript, jQuery, Bootstrap, CSS, HTML, Github, Heroku",
+        sourceCode: "https://github.com/Rian1010/Shopping-List",
+        websiteURL: "https://shopping-list-google.herokuapp.com/"
+    },
+    {
+        img: "site-8",
+        title: "Charity Base (Second Milestone Project)",
+        titel: "Wohltätigkeiten Website",
+        technologies: "HTML, CSS, JavaScript, DC.js API, D3.js API, Email.js API, Google Maps API, Github",
+        sourceCode: "https://github.com/Rian1010/Charity-Website",
+        websiteURL: "https://rian1010.github.io/Charity-Website/"
+    },
+    {
+        img: "site-9",
+        title: "Portfolio",
+        titel: "Portfolio",
+        technologies: "ReactJS, JavaScript, Bootstrap, Responsive Web Design, HTML, CSS, Github",
+        sourceCode: "https://github.com/Rian1010/Portfolio",
+        websiteURL: "https://rian1010.github.io/Portfolio/"
+    },
+    {
+        img: "site-10",
+        title: "Thorin & Company",
+        titel: "Thorin & Die Truppe",
+        technologies: "Flask, Python, AJAX, jQuery, JavaScript, Bootstrap, HTML, CSS, SASS, Github, Heroku",
+        sourceCode: "https://github.com/Rian1010/Flask",
+        websiteURL: "https://thorin-and-company-4.herokuapp.com/"
+    },
+    {
+        img: "site-11",
+        title: "Germany Online Guide (First Milestone Project)",
+        titel: "Deutschland Online-Guide (Erstes Meilenstein Projekt)",
+        technologies: "HTML, CSS, Bootstrap, JavaScripts, Github",
+        sourceCode: "https://github.com/Rian1010/Germany-Online-Guide",
+        websiteURL: "https://rian1010.github.io/Germany-Online-Guide/index.html"
+    }
 ];
+
 
 export default fullPortfolio;
